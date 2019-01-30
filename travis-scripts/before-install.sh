4d74f35bffae044925408b12aaa698a9d9854f03
devefx <devefx@163.com>
2019-01-30 19:09:04 +0800

    fix some bug

--- travis-scripts/before-install.sh
+++ travis-scripts/before-install.sh
@@ -83,10 +83,15 @@ function install_python_module_for_osx()
 
 function install_python_module_for_windows()
 {
+    mkdir -p $HOME/bin
+    pushd $HOME/bin
+
     PIP_DOWNLOAD_URL="https://bootstrap.pypa.io/get-pip.py"
     echo "Download ${PIP_DOWNLOAD_URL}"
     ${CURL} -0 ${PIP_DOWNLOAD_URL}
     python get-pip.py
+    popd
+    echo "Installing python modules finished!"
 }
 
 function install_latest_python()
